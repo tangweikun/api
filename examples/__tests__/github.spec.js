2ce4abf140edaafc41dbcf4f16363e0325e36ed7
tangweikun <819573105@qq.com>
2018-05-09 13:03:39 +0800

    :white_check_mark: Update test case

---
+++ examples/__tests__/github.spec.js
@@ -0,0 +1,15 @@
+import { github } from 'api-collection'
+
+describe('github', () => {
+  it('followers', () => {
+    github('followers', { username: 'tangweikun' }).then(data =>
+      console.log('followers:\n', data),
+    )
+  })
+
+  it('userInfo', () => {
+    github('userInfo', { username: 'tangweikun' }).then(data => {
+      console.log('userInfo:\n', data)
+    })
+  })
+})
